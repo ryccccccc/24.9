13478bb7af807df2c8e9281e48b115ae3a240cb4
R1cky <R1cky@PC-RICKY>
2023-12-29 11:42:06 +0100

    Aggiungere i file di progetto.

---
+++ 24.9/Program.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24._9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Vector v1;
+            v1.x1 = 2;
+            v1.y1 = 2;
+            Vector v2;
+            v2.x1 = 3;
+            v2.y1 = 3;
+        }
+    }
+}
---
+++ 24.9/Vector.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace _24._9
+{
+    internal class Vector
+    {
+        public float Modulo { get; set; }
+        public float x1 { get; set; }
+        public float y1 { get; set; }
+
+        public Vector(float X1, float Y1)
+        {
+            x1 = X1;
+            y1 = Y1;
+        }
+
+        public void Somma(float x2, float y2, out float xr, out float yr)
+        {
+            xr = x1 + x2;
+            yr = y1 + y2;
+        }
+
+        public static Vector Parse(float x3, float y3)
+        {
+            
+        }
+    }
+}
